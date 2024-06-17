9615c80896dfb8bca2b583bab33ec937d18e2d02
vitor <vito.velloso@gmail.com>
2024-06-17 18:02:49 -0300

    adição de notas

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "Vitor"; // texto
+        char sexo = 'M'; // caractér de 1 letra apenas
+        boolean doadorOrgao = false; // valores como Sim e Não
+        Date dataNascimento = new Date();
+
+        // Aritméticos
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        short numeroCurto = 1; //short é um numero curto, que ocupa pouca memoria//
+        int numeroNormal = numeroCurto; //int é um numero normal para os dias de hoje, é mais comum//
+        short numeroCurto2 = (short) numeroNormal; 
+        //essas 3 linhas mostram que um comando de short pode caber dentro de int, mas um short n pode querer puxar um numero categorizado como int, pq o numero pode ser maior que o limite do short//
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        final double valorDePi = 3.14;
+
     }
 }
